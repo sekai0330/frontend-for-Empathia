5ab366a2dd3e2590d015d3b5da3cf22b191a0852
sekai0330 <asamieto965@gmail.com>
2024-04-20 04:02:59 +0900

    fix : voice input

--- src/sections/workflow.js
+++ src/sections/workflow.js
@@ -1,36 +1,32 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Container, Grid, Box, Heading, Text } from 'theme-ui';
-import SectionHeader from 'components/section-header';
+import { jsx } from "theme-ui";
+import { Container, Grid, Box, Heading, Text } from "theme-ui";
+import SectionHeader from "components/section-header";
 
-import PatternBG from 'assets/patternBG.png';
-import ArrowOdd from 'assets/arrowOdd.svg';
-import ArrowEven from 'assets/arrowEven.svg';
+import PatternBG from "assets/patternBG.png";
+import ArrowOdd from "assets/arrowOdd.svg";
+import ArrowEven from "assets/arrowEven.svg";
 
 const data = [
   {
     id: 1,
-    title: 'Create an account',
-    text:
-      'Sign up if you do not have an account to access our AI counselling product. ',
+    title: "Create an account",
+    text: "Sign up if you do not have an account to access our AI counselling product. ",
   },
   {
     id: 2,
-    title: 'Talk to our AI therapist',
-    text:
-      'Our AI has been designed to provide an answer to virtually anything you ask it.',
+    title: "Talk to our AI therapist",
+    text: "Our AI has been designed to provide an answer to virtually anything you ask it.",
   },
   {
     id: 3,
-    title: 'Receive feedback',
-    text:
-      'After you talk to our AI, it will provide you with a score and feedback for you to improve!',
+    title: "Receive feedback",
+    text: "After you talk to our AI, it will provide you with a score and feedback for your health to improve!",
   },
   {
     id: 4,
-    title: 'Repeat and progress',
-    text:
-      'See previous results in your dashboard and use the product over time to see your progression!',
+    title: "Repeat and progress",
+    text: "See previous results in your dashboard and use the product over time to see your progression!",
   },
 ];
 
@@ -62,91 +58,91 @@ export default function WorkFlow() {
 
 const styles = {
   workflow: {
-    backgroundColor: 'primary',
+    backgroundColor: "primary",
     backgroundImage: `url(${PatternBG})`,
     backgroundRepeat: `no-repeat`,
-    backgroundPosition: 'center center',
-    backgroundSize: 'cover',
-    position: 'relative',
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+    position: "relative",
     py: [8, null, 9, null, null, 10],
   },
   grid: {
     mb: -1,
     pt: 0,
     gridGap: [
-      '35px 0',
+      "35px 0",
       null,
-      '45px 30px',
+      "45px 30px",
       null,
-      '50px 25px',
+      "50px 25px",
       null,
       null,
-      '50px 65px',
+      "50px 65px",
     ],
     gridTemplateColumns: [
-      'repeat(1,1fr)',
+      "repeat(1,1fr)",
       null,
-      'repeat(2,1fr)',
+      "repeat(2,1fr)",
       null,
-      'repeat(4,1fr)',
+      "repeat(4,1fr)",
     ],
   },
   card: {
-    display: 'flex',
-    flexDirection: 'column',
-    position: 'relative',
-    textAlign: ['center', null, 'left'],
-    width: ['100%', '80%', '100%'],
-    mx: ['auto'],
+    display: "flex",
+    flexDirection: "column",
+    position: "relative",
+    textAlign: ["center", null, "left"],
+    width: ["100%", "80%", "100%"],
+    mx: ["auto"],
     px: [4, null, null, 0],
-    '&::before': {
-      position: 'absolute',
+    "&::before": {
+      position: "absolute",
       content: '""',
       top: 0,
       left: [0, null, null, null, null, 72, null, 90],
       backgroundRepeat: `no-repeat`,
-      backgroundPosition: 'center center',
+      backgroundPosition: "center center",
       width: 215,
       height: 60,
-      '@media screen and (max-width:1220px)': {
-        display: 'none',
+      "@media screen and (max-width:1220px)": {
+        display: "none",
       },
     },
-    '&:nth-of-type(2n-1)::before': {
+    "&:nth-of-type(2n-1)::before": {
       backgroundImage: `url(${ArrowOdd})`,
     },
-    '&:nth-of-type(2n)::before': {
+    "&:nth-of-type(2n)::before": {
       backgroundImage: `url(${ArrowEven})`,
       top: 17,
     },
-    '&:last-child::before': {
-      display: 'none',
+    "&:last-child::before": {
+      display: "none",
     },
   },
 
   iconBox: {
-    width: ['50px', null, '60px', null, null, '70px'],
-    height: ['50px', null, '60px', null, null, '70px'],
+    width: ["50px", null, "60px", null, null, "70px"],
+    height: ["50px", null, "60px", null, null, "70px"],
     flexShrink: 0,
     borderRadius: [15, null, 23, null, null, 30],
-    backgroundColor: 'white',
-    display: 'flex',
-    alignItems: 'center',
+    backgroundColor: "white",
+    display: "flex",
+    alignItems: "center",
     mb: [5, null, null, null, null, 6],
-    mx: ['auto', null, 0],
-    fontSize: [6, null, 7, null, null, '30px'],
+    mx: ["auto", null, 0],
+    fontSize: [6, null, 7, null, null, "30px"],
     fontWeight: 700,
-    justifyContent: 'center',
-    color: '#234582',
+    justifyContent: "center",
+    color: "#234582",
   },
   wrapper: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    mt: '-5px',
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    mt: "-5px",
     title: {
       fontSize: [3, null, 4, null, null, 5],
-      color: 'white',
+      color: "white",
       lineHeight: [1.4, null, null, null, null, 1.55],
       pr: [0, null, null, null, null, 2],
       mb: [2, 3],
@@ -156,7 +152,7 @@ const styles = {
       fontSize: 1,
       fontWeight: 400,
       lineHeight: [1.85, null, null, 1.9, 2],
-      color: 'white',
+      color: "white",
       opacity: 0.75,
       pr: [0, null, null, null, null, 5],
     },
